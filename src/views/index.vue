f47cbad8589e9817c75a3fb774005f73eac01267
IMsuperM <1459322066@qq.com>
2024-07-31 18:14:44 +0800

    首页统计

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,18 +1,157 @@
 <template>
     <div class="app-container home">
-        <h1></h1>
+        <div class="chart-container" ref="chartLineRef"></div>
+        <div class="chart-container pie" ref="chartPieRef"></div>
     </div>
 </template>
 
 <script setup name="Index">
-const version = ref('3.8.8')
-function goTarget(url) {
-    window.open(url, '__blank')
+import * as echarts from 'echarts'
+
+const chartLineRef = ref(null) // 折线图
+const chartPieRef = ref(null) // 附带饼图
+let chartLine = null
+let chartPie = null
+
+function initChartLine() {
+    chartLine = echarts.init(chartLineRef.value)
+
+    // 配置项
+    const options = {
+        xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+                data: [820, 932, 901, 934, 1290, 1330, 1320],
+                type: 'line',
+                smooth: true
+            }
+        ]
+    }
+
+    // 设置图表配置项
+    chartLine.setOption(options)
 }
+function initChartPie() {
+    chartPie = echarts.init(chartPieRef.value)
+    const option = {
+        legend: {},
+        tooltip: {
+            trigger: 'axis',
+            showContent: false
+        },
+        dataset: {
+            source: [
+                ['product', '2012', '2013', '2014', '2015', '2016', '2017'],
+                ['Milk Tea', 56.5, 82.1, 88.7, 70.1, 53.4, 85.1],
+                ['Matcha Latte', 51.1, 51.4, 55.1, 53.3, 73.8, 68.7],
+                ['Cheese Cocoa', 40.1, 62.2, 69.5, 36.4, 45.2, 32.5],
+                ['Walnut Brownie', 25.2, 37.1, 41.2, 18, 33.9, 49.1]
+            ]
+        },
+        xAxis: { type: 'category' },
+        yAxis: { gridIndex: 0 },
+        grid: { top: '55%' },
+        series: [
+            {
+                type: 'line',
+                smooth: true,
+                seriesLayoutBy: 'row',
+                emphasis: { focus: 'series' }
+            },
+            {
+                type: 'line',
+                smooth: true,
+                seriesLayoutBy: 'row',
+                emphasis: { focus: 'series' }
+            },
+            {
+                type: 'line',
+                smooth: true,
+                seriesLayoutBy: 'row',
+                emphasis: { focus: 'series' }
+            },
+            {
+                type: 'line',
+                smooth: true,
+                seriesLayoutBy: 'row',
+                emphasis: { focus: 'series' }
+            },
+            {
+                type: 'pie',
+                id: 'pie',
+                radius: '30%',
+                center: ['50%', '25%'],
+                emphasis: {
+                    focus: 'self'
+                },
+                label: {
+                    formatter: '{b}: {@2012} ({d}%)'
+                },
+                encode: {
+                    itemName: 'product',
+                    value: '2012',
+                    tooltip: '2012'
+                }
+            }
+        ]
+    }
+    chartPie.on('updateAxisPointer', function (event) {
+        const xAxisInfo = event.axesInfo[0]
+        if (xAxisInfo) {
+            const dimension = xAxisInfo.value + 1
+            chartPie.setOption({
+                series: {
+                    id: 'pie',
+                    label: {
+                        formatter: '{b}: {@[' + dimension + ']} ({d}%)'
+                    },
+                    encode: {
+                        value: dimension,
+                        tooltip: dimension
+                    }
+                }
+            })
+        }
+    })
+    chartPie.setOption(option)
+}
+onMounted(() => {
+    initChartLine()
+    initChartPie()
+})
+
+onUnmounted(() => {
+    if (chartLine) {
+        chartLine.dispose()
+        chartLine = null
+    }
+    if (chartPie) {
+        chartPie.dispose()
+        chartPie = null
+    }
+})
 </script>
 
 <style scoped lang="scss">
 .home {
+    display: flex;
+    .chart-container {
+        width: 400px;
+        height: 300px;
+    }
+    .pie {
+        flex: 1;
+        height: 500px;
+        overflow: auto;
+        // width: 800px;
+    }
+
     blockquote {
         padding: 10px 20px;
         margin: 0 0 20px;
